e36eb856dea6c18b735754647454ed259e25e6e4
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2012-06-22 09:36:09 +0000

    some example use of common extern libs

---
+++ example/pcl/v03-kinect2file2eucledeanCluster/part1-creation/main.cpp
@@ -0,0 +1,52 @@
+
+#include <pcl/io/openni_grabber.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+#include <pcl/io/pcd_io.h>  //j//
+
+#ifdef _WIN32
+#define sleep(x) Sleep((x)*1000)
+#endif
+
+class SimpleOpenNIViewer {
+public:
+    SimpleOpenNIViewer () : viewer ("PCL OpenNI Viewer") {}
+
+    void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud) {
+        if (!viewer.wasStopped()) {
+            viewer.showCloud (cloud);
+            writer.write<pcl::PointXYZ> ("kinect_cloud.pcd", *cloud, false); //*
+        }
+    }
+
+    void run () {
+
+        pcl::Grabber* interface = new pcl::OpenNIGrabber();
+
+        boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f =
+            boost::bind (&SimpleOpenNIViewer::cloud_cb_, this, _1);
+
+        interface->registerCallback (f);
+
+        interface->start ();
+
+        while (!viewer.wasStopped()) {
+            boost::this_thread::sleep (boost::posix_time::seconds (1));
+            printf("alive...\n");
+        }
+
+        interface->stop ();
+    }
+
+    pcl::visualization::CloudViewer viewer;
+
+    pcl::PCDWriter writer;  //j//
+
+};
+
+int main () {
+    SimpleOpenNIViewer v;
+    v.run ();
+    return 0;
+}
+
