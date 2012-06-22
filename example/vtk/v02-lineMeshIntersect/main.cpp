2b343a72a57dacf33ff74ae4e9f16cbd5278c371
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2012-06-22 10:33:56 +0000

    as name indicates

---
+++ example/vtk/v02-lineMeshIntersect/main.cpp
@@ -0,0 +1,40 @@
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
+#include <vtkPoints.h>
+#include <vtkPolyData.h>
+#include <vtkPointData.h>
+#include <vtkLine.h>
+#include <vtkOBBTree.h>
+ 
+int main(int, char *[])
+{
+  vtkSmartPointer<vtkSphereSource> sphereSource = 
+    vtkSmartPointer<vtkSphereSource>::New();
+  sphereSource->Update();
+ 
+  // Create the locator
+  vtkSmartPointer<vtkOBBTree> tree = 
+    vtkSmartPointer<vtkOBBTree>::New();
+  tree->SetDataSet(sphereSource->GetOutput());
+  tree->BuildLocator();
+ 
+  // Intersect the locator with the line
+  double lineP0[3] = {0.0, 0.0, 0.0};
+  double lineP1[3] = {0.0, 0.0, 2.0};
+  vtkSmartPointer<vtkPoints> intersectPoints = 
+    vtkSmartPointer<vtkPoints>::New();
+ 
+  tree->IntersectWithLine(lineP0, lineP1, intersectPoints, NULL);
+ 
+  double intersection[3];
+  intersectPoints->GetPoint(0, intersection);
+  std::cout << "NumPoints: " << intersectPoints->GetNumberOfPoints()
+            << std::endl;
+  std::cout << "Intersection: " 
+            << intersection[0] << ", " 
+            << intersection[1] << ", "
+            << intersection[2] << std::endl;
+ 
+  return EXIT_SUCCESS;
+}
+
