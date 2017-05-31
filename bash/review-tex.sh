bc0954204777c785b91193b574fdf60686e27c6d
Juan G. Victores <jgvictores@users.noreply.github.com>
2017-05-31 12:22:04 +0200

    Update review-tex.sh

--- bash/review-tex.sh
+++ bash/review-tex.sh
@@ -7,6 +7,10 @@ grep "\.\." *.tex | grep -v "\.\.\." | grep -v "\.\.\/"  # sed: \.
 grep "This way" *.tex  # sed: omit
 grep "But" *.tex  # sed: omit or replace by however
 
+#
+grep -i "do" *.tex # sed: perform
+grep -i "also" *.tex # sed: additionally
+
 # 
 grep -i "problem" *.tex # sed: issue, challenge
-grep -i "do" *.tex # sed: perform
+
