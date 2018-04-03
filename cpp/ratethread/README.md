d5e93ccbf58499d378c81f7468b24a15b78695d1
jgvictores <jcgvicto@gmail.com>
2018-04-03 22:49:35 +0200

    mv description to README

---
+++ cpp/ratethread/README.md
@@ -0,0 +1,7 @@
+# RateThread
+
+Partial inspiration:
+- http://thispointer.com/c-11-multithreading-part-1-three-different-ways-to-create-threads/
+- https://bulldozer00.com/2013/12/27/periodic-processing-with-standard-c11-facilities/
+   - Copied at: https://gist.github.com/catnapgames/ac0a9060c9d5d637b452f9ee15a6a466
+- Some naming conventions from https://github.com/robotology/yarp RateThread class
--- cpp/ratethread/RateThread.hpp
+++ cpp/ratethread/RateThread.hpp
@@ -2,12 +2,6 @@
 // CopyPolicy: released under the terms of the LGPLv2.1
 // URL: https://github.com/jgvictores/snippets
 
-// Help (partial inspiration):
-// - http://thispointer.com/c-11-multithreading-part-1-three-different-ways-to-create-threads/
-// - https://bulldozer00.com/2013/12/27/periodic-processing-with-standard-c11-facilities/
-//     - Copied at: https://gist.github.com/catnapgames/ac0a9060c9d5d637b452f9ee15a6a466
-// - Some naming conventions from https://github.com/robotology/yarp RateThread class
-
 #include <chrono>
 #include <thread>
 
