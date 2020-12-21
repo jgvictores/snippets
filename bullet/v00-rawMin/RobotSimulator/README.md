3c9c000cad5afe983d7237823250727d3a6301ae
jgvictores <jcgvicto@gmail.com>
2020-12-21 11:41:29 +0100

    bullet minimal example with GUI

---
+++ bullet/v00-rawMin/RobotSimulator/README.md
@@ -0,0 +1,6 @@
+# RobotSimulator
+
+Origin of files:
+
+- <https://github.com/bulletphysics/bullet3/tree/master/examples/RobotSimulator>
+- <https://github.com/bulletphysics/bullet3/tree/b1f76960ff630969050f90784b1f254ad6a7ded8/examples/RobotSimulator>
--- bullet/v00-rawMin/main.cpp
+++ bullet/v00-rawMin/main.cpp
@@ -1,6 +1,16 @@
-#include <btBulletDynamicsCommon.h>
+// thanks: bullet3/examples/RobotSimulator/RobotSimulatorMain.cpp
 
-int main (int argc, char * const argv[])
+#include "RobotSimulator/b3RobotSimulatorClientAPI.h"
+//#include "b3RobotSimulatorClientAPI.h"
+
+#include <unistd.h>
+
+int main(int argc, char* argv[])
 {
+    b3RobotSimulatorClientAPI* sim = new b3RobotSimulatorClientAPI();
+
+	bool isConnected = sim->connect(eCONNECT_GUI);
+
+    usleep(2000000);
     return 0;
 }
